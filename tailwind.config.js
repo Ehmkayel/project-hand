4bd28e1e3e3287d665ba225faa25b0d21f4221b4
lamidimorufat <lamidimorufat0@gmail.com>
2024-06-27 01:45:23 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        yellow: "#FEC00A",
+        lightYellow: "#FDCA09",
+        black: "#101010",
+        white: "#E7E7E7",
+        gray: "#5A6675",
+        lightGray: "#525D6A",
+
+      },
+      backgroundImage: {
+        
+      },
+      screens: {
+        sm: '400px',
+        md: '768px',
+        lg: '1200px',
+        xl: '1440px'
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+ 
+     
+      
+    
+
